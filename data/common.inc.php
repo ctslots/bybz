8179abf60309b960f82b9cc6990b850eabd7fc5a
yueyue <shbozb.com>
2017-05-20 15:33:51 +0800

    waterskygg

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+﻿<?php
+//数据库连接信息
+$cfg_dbhost = 'sql313.summerhost.info';
+$cfg_dbname = 'sum_20086909_boyiweb';
+$cfg_dbuser = 'sum_20086909';
+$cfg_dbpwd = 'a52290120a';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
